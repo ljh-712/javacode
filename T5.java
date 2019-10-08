665e19a050409bf03ff443b166ce614246ffcf53
ljh-712 <l15991864531@163.com>
2019-10-08 22:44:44 +0800

    求5！和求1！+2！+3！+4！+5！

---
+++ T5.java
@@ -0,0 +1,32 @@
+public class T5 {
+	/*public static void main2(String []args){
+		int num=5;
+		int i=1;
+		int ret=1;
+		while(i<=num){
+			ret=ret*i;
+			i++;
+		}
+		System.out.println(ret);
+	}
+}
+	
+	public static void main1(String []args){
+		int num=5;
+		int ret=1;
+		int sum=0;
+		int i=1;
+		while(i<=num){
+			int j=1;
+			ret=1;
+			while(j<=i){
+				ret=ret*j;
+				j++;
+			}
+			sum=sum+ret;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
+	*/
