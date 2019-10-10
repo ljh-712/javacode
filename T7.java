4788d3c9a05deea7ceea8ea4142ff10d86b15ce8
ljh-712 <l15991864531@163.com>
2019-10-10 22:05:43 +0800

    打印99乘法表

---
+++ T7.java
@@ -0,0 +1,12 @@
+	
+public class T7{
+public static void main4(String [] args){
+		int ret=0;
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(" "+j+"*"+i+"="+i*j);
+			}
+				System.out.println();
+		}
+	}
+}
